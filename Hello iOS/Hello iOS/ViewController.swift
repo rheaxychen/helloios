24238fda72b2edccf9cdf3aa1cec571978eb751f
Rhea Xinyu Chen <xinyuc2@uw.edu>
2019-01-09 19:47:08 -0800

    Add assignment 1

---
+++ Hello iOS/Hello iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello iOS
+//
+//  Created by Rhea on 1/9/19.
+//  Copyright © 2019 Rhea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # helloios
-assignment 1 for info 449 winter 2019 at University of Washington
+
+Assignment 1 for info 449 winter 2019 at University of Washington
+- Author: Rhea Chen (Xinyu)
+
+### References
+
+App Icon is Color #A3CCE2 from [Picular London](https://picular.co/london)
+
